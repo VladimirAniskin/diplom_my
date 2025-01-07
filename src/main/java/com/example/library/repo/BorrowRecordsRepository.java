5f79f38b062585c2743c05ddce9d4acddf5fd52a
Владимир <vova1210@yandex.ru>
2025-01-07 15:49:06 +0300

    diplom

--- src/main/java/com/example/library/repo/BorrowRecordsRepository.java
+++ src/main/java/com/example/library/repo/BorrowRecordsRepository.java
@@ -2,11 +2,19 @@ package com.example.library.repo;
 
 import com.example.library.mod.BorrowRecords;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
 /**
  * Интерфейс BorrowRecordsRepository, предоставляющий методы для работы с сущностью BorrowRecords.
  * Расширяет JpaRepository для использования стандартных операций CRUD.
  */
 @Repository
 public interface BorrowRecordsRepository extends JpaRepository<BorrowRecords, Long> {
+    @Query("SELECT r FROM BorrowRecords r WHERE r.returnDate = :targetDate")
+    List<BorrowRecords> findBorrowRecordsByReturnDate(@Param("targetDate") LocalDate targetDate);
 }
--- src/main/java/com/example/library/service/BorrowRecordService.java
+++ src/main/java/com/example/library/service/BorrowRecordService.java
@@ -104,20 +104,16 @@ public class BorrowRecordService {
      */
     @Scheduled(cron = "0 0 7 * * ?") // Каждое утро в 7:00
     public void returnBook() {
-        LocalDate today = LocalDate.now();
-        LocalDate threeDaysFromNow = today.plusDays(3);
-        // Получаем все записи о выдаче книг
-        List<BorrowRecords> borrowRecords = borrowRecordsRepository.findAll();
+        LocalDate currentDate = LocalDate.now();
+        LocalDate targetDate = currentDate.plusDays(3); // Дата через 3 дня
+        // Извлечение всех записей о выдаче книг
+        List<BorrowRecords> borrowRecords = borrowRecordsRepository.findBorrowRecordsByReturnDate(targetDate);
         for (BorrowRecords record : borrowRecords) {
-            LocalDate returnDate = record.getReturnDate();
-            // Проверяем, если срок возврата книги истекает через 3 дня
-            if (returnDate != null && returnDate.equals(threeDaysFromNow)) {
-                User user = record.getUserId(); // Получаем пользователя
-                Book book = record.getBookId(); // Получаем книгу
-                // Отправляем уведомление через Kafka
-                kafkaTemplate.send("return-book", "Напоминание: Срок сдачи книги с ID " +
-                        book.getId() + " для пользователя с ID " + user.getId() + " истекает через 3 дня.");
-            }
+            User user = record.getUserId(); // Получение пользователя
+            Book book = record.getBookId(); // Получение книги
+            // Отправка уведомления через Kafka
+            kafkaTemplate.send("return-book", "Напоминание: Срок сдачи книги с ID " +
+                    book.getId() + " для пользователя с ID " + user.getId() + " истекает через 3 дня.");
         }
     }
 }
